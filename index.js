22fe06137532928df0d608f202eed9ad3a2d3209
Cameron Rushing <cameronrushing54@gmail.com>
2021-07-29 21:23:50 -0400

    index js 4

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const inquirer = require ("inquirer");
 require("console.table");
 // const sql = require("./sql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "employeesDB"
+const connection = mysql.createConnection({
+    host :"localhost",
+    port:3306,
+    user:"root",
+    password: "April261995$",
+    database:"employee_management_system_db",
+
 });
+
 
 // connect to the mysql server and sql database
 connection.connect(function (err) {
